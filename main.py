e4fed31aedc26c0945ec01e77f96fffcca9301fd
Mike <mikealexx97@gmail.com>
2024-10-09 21:44:51 +0300

    fixed leak ;)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-from transformers import pipeline
+import requests, os
 import logging
 from neonize.client import NewClient
 from neonize.events import (
@@ -8,9 +7,12 @@ from neonize.events import (
 )
 
 # Initializations
-whisper = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
+whisper_url = "https://api-inference.huggingface.co/models/openai/whisper-large-v3"
 client = NewClient("just_type_bro.sqlite3")
 logger = logging.getLogger("Whisper")
+
+api_token = os.getenv("api_token")
+headers = {"Authorization": f"Bearer {api_token}"}
 
 if not os.path.exists('temp'):
     os.makedirs('temp')
@@ -32,8 +34,23 @@ def handler(client, message):
     opus_path = 'media/audio.opus'
     mp3_path = 'media/audio.mp3'
     os.system(f'ffmpeg -i "{opus_path}" -vn "{mp3_path}" -y')
-    transcription = whisper(mp3_path)
-    client.send_message(chat, transcription)
+    
+    with open(mp3_path, 'rb') as f:
+        data = f.read()
+
+    # Send a POST request to the API with the audio data
+    response = requests.post(whisper_url, headers=headers, data=data)
+
+    # Check the response status
+    if response.status_code == 200:
+        # Parse and print the JSON response
+        transcription = response.json()["text"]
+        client.send_message(chat, transcription)
+    else:
+        client.send_message(chat, "Whisper service is currently unavailable. Please try again later.")
+
+    os.remove(opus_path)
+    os.remove(mp3_path)
 
 if __name__ == "__main__":
     client.connect()
